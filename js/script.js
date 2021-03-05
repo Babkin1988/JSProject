cd451cbc29e86e01df7359a16f09f4bf44e12be3
Aleksey Babkin <babkinps@mail.ru>
2021-03-05 21:54:34 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+console.log(111);
+let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+console.log(number);
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj); {
+    let result = 100;
+}
+
+/*
+*/
+console.log(4/0);
+const person = "Alex";
+console.log(person);
+
+
+const boo = true;
+let something;
+console.log(something);
+
+const obj1 = {
+    value: 10,
+    name: "Ivan"
+};
+
+console.log(obj1.name); 
+
+let arr = [1, 2, 4, 12];
+const category = "toys";
+console.log(`http://someurl.com/${category}`);

471e0fbe0ca24e4a9cd06e4ce1ce409a821c8d7c
Aleksey Babkin <babkinps@mail.ru>
2021-03-07 18:37:04 +0300

    moviesapp

--- js/script.js
+++ js/script.js
@@ -1,37 +1,19 @@
-"use strict";
-
-console.log(111);
-let number = 5;
-const leftBorderWidth = 1;
-number = 10;
-console.log(number);
-const obj = {
-    a: 50
+const numberOfFilms = +prompt('How many movies did you watch before?', '');
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    private : false
 };
 
-obj.a = 10;
-console.log(obj); {
-    let result = 100;
-}
+const movieName1 = prompt('Movie name', ''),
+rank1 = +prompt('Rank', ''),
+movieName2 = prompt('Movie name', ''),
+rank2 = +prompt('Rank', '');
 
-/*
-*/
-console.log(4/0);
-const person = "Alex";
-console.log(person);
+console.log(personalMovieDB);
 
 
-const boo = true;
-let something;
-console.log(something);
-
-const obj1 = {
-    value: 10,
-    name: "Ivan"
-};
-
-console.log(obj1.name); 
-
-let arr = [1, 2, 4, 12];
-const category = "toys";
-console.log(`http://someurl.com/${category}`);
+personalMovieDB.movies[ movieName1] = rank1;
+personalMovieDB.movies[ movieName2] = rank2;
